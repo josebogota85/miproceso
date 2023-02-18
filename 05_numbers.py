7498b186ad1c6a6c242bf06b3c7d8182ce88fff6
Jose Luis Rincón Ortiz <47236318+josebogota85@users.noreply.github.com>
2023-02-18 02:27:22 +0000

    Este es el ejercicio con números, para trabajar con ellos.

---
+++ 05_numbers.py
@@ -0,0 +1,31 @@
+lives=3
+print(lives)
+print(type(lives))
+age=25
+buget=1000
+
+#Los números flotantes son los que que tienen una parte entera y una parte decimal, como pi 3.1416.., para estos números el formato es float, y se declaran igual que los enteros
+
+temperature=12.12
+print(temperature)
+print(type(temperature))
+
+lives=12+15
+print(lives)
+
+lives=lives-1
+print(lives)
+
+
+lives+=1
+print(lives, "Acá se le sumo uno a lo que traía lives")
+
+lives-=1
+print(lives, "Acá se ke resto uno a lo que traía lives")
+
+
+number=420000000000000000000.1
+print(number)
+
+number_b=00000000000000000.12
+print(number_b)
