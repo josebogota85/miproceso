fbc71978a20f3147da142e755e6ce654cbe4d643
Jose Luis Rincón Ortiz <47236318+josebogota85@users.noreply.github.com>
2023-02-20 20:47:14 +0000

    En esta actualización trabajamos en los operadores de comparación

---
+++ 09_comparators.py
@@ -0,0 +1,31 @@
+ #Los operadores de comparación son operadores que nos sirven como su nombre lo indica para comparar dos o mas valores, esta comparación generalmente nos produce, True o False como resultado
+print(7>3)# En este ejemplo nos va a dar un resultado True, debido a que 7 , si es mayor a 3
+print(3>7)# En este caso dará False por que 3 no es mayor a 7
+print(7>7)#En este caso también dará False por que 7 no es mayor que 7
+#-----------------------//---------------------------------
+
+#En el caso de menor que se usa de la siguiente manera
+print(7<3) #False
+print(7<14) #True
+print(7<7) #False
+
+#También podemos usar mayor o igual o menor o igual de la siguiente manera
+print(2>=1) #True
+print(2>=3)  #False
+print(2>=2)  #True
+
+print(1<=2) # True
+print(2<=1) # FAlse
+print(2<=2) # TRue
+
+#Para usar la comparción de igual que, se deben usar signos de igual uno al lado del otro de la siguiente manera ==
+
+print(6==6)#True
+print(5==2)#False
+
+#Para usar el operador diferete que se debe usar el signo de admiración y de igual uno al lado del otro de la siguiente manera !=
+print(6!=10)  #True
+print(6!=6)  #False
+
+
+
