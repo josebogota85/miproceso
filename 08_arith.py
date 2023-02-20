288c8c5ff1a7a830d75d55b3d76bf2e734d93c6b
Jose Luis Rincón Ortiz <47236318+josebogota85@users.noreply.github.com>
2023-02-20 05:20:58 +0000

    Operadores aritméticos

---
+++ 08_arith.py
@@ -0,0 +1,23 @@
+#suma
+print(10+1)
+#resta
+print(10-8)
+#multiplicación
+print(8*5)
+#División
+print(74/7)
+#Módulo
+print(9%4)
+#Parte entera, me muestra solo la parte entera de la división
+print(9//4)
+#Potenciación
+print(2**5)
+
+
+#Los operadores aritmeticos también me pérmiten operar otros tipos de datos por ejemplo vamos a concatenar usando el signo +
+print('HOla'+' Mundo!')
+
+#También podemos multiplicar otro tipo de dato, como un string de la siguiente manera
+print(' Hola ' * 3)
+
+
