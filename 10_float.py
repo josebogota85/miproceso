2ecf814e54760a80e7c14debc9a8475dbae882fd
Jose Luis Rincón Ortiz <47236318+josebogota85@users.noreply.github.com>
2023-02-21 20:25:23 +0000

    Acá tenemos una actualización tanto para la función types, comp para la transformación de datos y la comparación de flotantes

--- 10_float.py
+++ 10_float.py
@@ -4,3 +4,16 @@ print(x)
 y=1.1 + 2.2
 print(y)
 print(x==y)
+
+#Para arreglar los decimales de un numero se puede hacer de dos maneras, se le puede dar formato usando la función format, como el siguiente ejemplo, pero ojo esto lo deja en formato string
+
+y_formato_string=format(y,"2g")
+print('string => ', y_formato_string)
+print(y_formato_string==str(x))
+
+print("_"*20)#Esto se usa como un separador
+
+#Tambíen se puede dar formato al núnmero decimal de una forma más matématica, para no tener que cambiar el formato entero del nuúmero, para usar esta forma matemática es necesario definir una tolerancia, ojo al ejemplo
+
+tolerancia_diferencia=0.00001
+print(abs(x-y)<tolerancia_diferencia)
